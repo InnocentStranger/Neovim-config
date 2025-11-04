b35714eb4fb6e6283f40f204693ff891f0bc0115
Aryan Rajput <aryanrajput644@gmail.com>
2025-11-04 23:32:42 +0530

    added java lsp

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,34 +1,34 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  opts = {
-    -- Mason Downloads & automatically enable listed lsp's (for manually enable lsp, download the lsp and enable it using vim.lsp.enable(<lsp-name>))
-    -- Name should be same as present in neovim/nvim-lspconfig
-    ensure_installed = {
-      "ts_ls",
-      "html",
-      "cssls",
-      "tailwindcss",
-      "lua_ls",
-      "graphql",
-      "rust_analyzer",
-      "jdtls",
-      "clangd",
-    },
-  },
-  dependencies = {
-    {
-      "williamboman/mason.nvim",
-      opts = {
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      },
-    },
-    -- this github repo contains all the lsp configuration files maintained by community
-    "neovim/nvim-lspconfig"
-  }
+	"williamboman/mason-lspconfig.nvim",
+	opts = {
+		-- Mason Downloads & automatically enable listed lsp's (for manually enable lsp, download the lsp and enable it using vim.lsp.enable(<lsp-name>))
+		-- Name should be same as present in neovim/nvim-lspconfig
+		ensure_installed = {
+			"ts_ls",
+			"html",
+			"cssls",
+			"tailwindcss",
+			"lua_ls",
+			"graphql",
+			"rust_analyzer",
+			-- "jdtls",
+			"clangd",
+		},
+	},
+	dependencies = {
+		{
+			"williamboman/mason.nvim",
+			opts = {
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			},
+		},
+		-- this github repo contains all the lsp configuration files maintained by community
+		"neovim/nvim-lspconfig",
+	},
 }
